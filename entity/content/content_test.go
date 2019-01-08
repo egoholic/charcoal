3a8af0f873e1624c1f8d45956c671608d45214e4
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2019-01-08 16:03:06 +0200

    adds more content tests

--- entity/content/content_test.go
+++ entity/content/content_test.go
@@ -14,19 +14,25 @@ var (
 var _ = Describe("Content Entity", func() {
 	Context("creation", func() {
 		Describe("New()", func() {
-			It("return pointer on Content", func() {
+			It("returns pointer on Content", func() {
 				Expect(New(title1, body1)).To(BeAssignableToTypeOf(&Content{}))
 			})
 		})
 	})
 	Context("accessors", func() {
-		Describe("Title()", func() {
+		Describe(".PK()", func() {
+			It("returns primary key", func() {
+				content := New(title1, body1)
+				Expect(content.PK()).To(Equal(title1))
+			})
+		})
+		Describe(".Title()", func() {
 			It("returns title", func() {
 				content := New(title1, body1)
 				Expect(content.Title()).To(Equal(title1))
 			})
 		})
-		Describe("AssignTitle()", func() {
+		Describe(".AssignTitle()", func() {
 			Context("when persisted", func() {
 				It("returns title", func() {
 					content := New(title1, body1)
@@ -43,13 +49,13 @@ var _ = Describe("Content Entity", func() {
 				})
 			})
 		})
-		Describe("Body()", func() {
+		Describe(".Body()", func() {
 			It("returns body", func() {
 				content := New(title1, body1)
 				Expect(content.Body()).To(Equal(body1))
 			})
 		})
-		Describe("AssignBody()", func() {
+		Describe(".AssignBody()", func() {
 			Context("when persisted", func() {
 				It("returns body", func() {
 					content := New(title1, body1)
@@ -61,6 +67,14 @@ var _ = Describe("Content Entity", func() {
 					content := New(title1, body1)
 					Expect(content.Body()).To(Equal(body1))
 				})
+			})
+		})
+		Describe(".Payload()", func() {
+			It("returns payload", func() {
+				content := New(title1, body1)
+				payload := content.Payload()
+				Expect(payload.Title()).To(Equal(title1))
+				Expect(payload.Body()).To(Equal(body1))
 			})
 		})
 	})
